ae7c1bc68036e573e766bbd6a1c86a672bd3cf67
abaudet <abaudet@sii.fr>
2015-06-22 22:07:24 +0200

    Fix navbar spy

--- js/site.js
+++ js/site.js
@@ -2,6 +2,8 @@ $(window).load(function() {
 	$('div.source pre, pre code').each(function(i, e) {
 		e.innerHTML = e.innerHTML.replace(/\n/g, "<br />").replace(/\t/g, "&nbsp;&nbsp;");
 	});
-	$("#toc-bar").affix({});
+	$('[data-spy="scroll"]').each(function () {
+		$(this).scrollspy('refresh');
+	});
 })
 
