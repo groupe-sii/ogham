317b9d878d68734762b537e7a67f375079de06a4
abaudet <abaudet@sii.fr>
2015-08-31 14:49:23 +0200

    Add Spring integration using Spring Boot auto-configuration

---
+++ ogham-spring/src/main/java/fr/sii/ogham/spring/autoconfigure/OghamAutoConfiguration.java
@@ -0,0 +1,58 @@
+package fr.sii.ogham.spring.autoconfigure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+
+import fr.sii.ogham.core.builder.MessagingBuilder;
+import fr.sii.ogham.core.service.MessagingService;
+import fr.sii.ogham.spring.config.PropertiesBridge;
+
+@Configuration
+@AutoConfigureAfter({WebMvcAutoConfiguration.class, ThymeleafAutoConfiguration.class})
+@ConditionalOnMissingBean(MessagingService.class)
+// TODO: manage all other template engines (freemarker, velocity, ...)
+public class OghamAutoConfiguration {
+	
+	@Bean
+	@ConditionalOnMissingBean
+	public PropertiesBridge propertiesBridge() {
+		return new PropertiesBridge();
+	}
+	
+	@Configuration
+	@ConditionalOnClass(SpringTemplateEngine.class)
+	public static class ThymeleafConfiguration {
+		@Autowired
+		Environment environment;
+		
+		@Bean
+		// TODO: use Spring message source and resource resolver too ?
+		public MessagingService messagingService(PropertiesBridge propertiesBridge, SpringTemplateEngine engine) {
+			MessagingBuilder builder = new MessagingBuilder().useAllDefaults(propertiesBridge.convert(environment));
+			builder.getEmailBuilder().getContentTranslatorBuilder().getTemplateBuilder().getThymeleafParser().withTemplateEngine(engine);
+			builder.getSmsBuilder().getContentTranslatorBuilder().getTemplateBuilder().getThymeleafParser().withTemplateEngine(engine);
+			return builder.build();
+		}
+	}
+	
+	@Configuration
+	@ConditionalOnMissingClass(SpringTemplateEngine.class)
+	public static class DefaultConfiguration {
+		@Autowired
+		Environment environment;
+		
+		@Bean
+		public MessagingService messagingService(PropertiesBridge propertiesBridge) {
+			return new MessagingBuilder().useAllDefaults(propertiesBridge.convert(environment)).build();
+		}
+	}
+}
---
+++ ogham-spring/src/main/java/fr/sii/ogham/spring/config/PropertiesBridge.java
@@ -0,0 +1,47 @@
+package fr.sii.ogham.spring.config;
+
+import java.util.Properties;
+
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertySource;
+
+/**
+ * <p>
+ * Spring {@link Environment} provides access to properties according to the
+ * current environment. But it doesn't provide any way to access all properties.
+ * </p>
+ * <p>
+ * This bridge converts Spring {@link Environment} into {@link Properties}.
+ * </p>
+ * 
+ * @see "https://jira.spring.io/browse/SPR-10241"
+ * 
+ * @author Aurélien Baudet
+ *
+ */
+public class PropertiesBridge {
+	/**
+	 * Convert the environment to properties. It only works on
+	 * {@link ConfigurableEnvironment} and with {@link PropertySource}s that
+	 * implement {@link EnumerablePropertySource}.
+	 * 
+	 * @param env
+	 *            the environment that handles properties in Spring
+	 * @return the properties for using it in Ogham
+	 */
+	public Properties convert(Environment env) {
+		Properties rtn = new Properties();
+		if (env instanceof ConfigurableEnvironment) {
+			for (PropertySource<?> propertySource : ((ConfigurableEnvironment) env).getPropertySources()) {
+				if (propertySource instanceof EnumerablePropertySource) {
+					for (String key : ((EnumerablePropertySource<?>) propertySource).getPropertyNames()) {
+						rtn.setProperty(key, String.valueOf(propertySource.getProperty(key)));
+					}
+				}
+			}
+		}
+		return rtn;
+	}
+}
