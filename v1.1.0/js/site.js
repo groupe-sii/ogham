65ecddef67604745effa76b988dcb4753830c01e
abaudet <abaudet@sii.fr>
2017-07-06 21:10:40 +0200

    site(version) Add version switcher on v1.1.0

--- v1.1.0/js/site.js
+++ v1.1.0/js/site.js
@@ -5,5 +5,15 @@ $(window).load(function() {
 	$('[data-spy="scroll"]').each(function () {
 		$(this).scrollspy('refresh');
 	});
+
+	var currentVersion = $('.projectVersion').text().replace(/Version: (.+)[.] $/, "$1");
+	var nav = $('.nav.pull-right');
+	nav.append('<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">v'+currentVersion+' <b class="caret"></b></a><ul class="dropdown-menu versions"></ul></li>');
+	var ul = nav.find('.dropdown-menu.versions');
+	$.get($(".brand").attr("href") + "/versions.json", function(data) {
+		data.forEach(function(version) {
+			ul.append('<li><a href="'+$(".brand").attr("href")+'/'+version+'/" title="'+version+'">'+version+'</a></li>');
+		});
+	});
 })
 
