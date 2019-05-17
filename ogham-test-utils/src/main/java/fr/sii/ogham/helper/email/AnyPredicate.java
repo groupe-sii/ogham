8c2c7d90e234ea97f39208c60f885ef4c85f34f9
kad D <kad@demkada.com>
2019-05-17 12:46:17 +0200

    Update AnyPredicate.java

--- ogham-test-utils/src/main/java/fr/sii/ogham/helper/email/AnyPredicate.java
+++ ogham-test-utils/src/main/java/fr/sii/ogham/helper/email/AnyPredicate.java
@@ -1,16 +1,10 @@
 package fr.sii.ogham.helper.email;
 
 import com.google.common.base.Predicate;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
 public class AnyPredicate<T> implements Predicate<T> {
 	@Override
 	public boolean apply(T input) {
 		return true;
-	}
-
-	@Override
-	public boolean test(@Nullable T input) {
-		return false;
 	}
 }
