3b52a317367d1d71d362fb34ea3aade005401532
abaudet <abaudet@sii.fr>
2019-06-26 02:29:51 +0400

    chore(ci) Use Github Pages to store docs and reports instead of surge
    surge.sh has a 50MB limit and delete all previous files before uploading.
    So it is not possible to keep history of branches.

--- ci/generate-documentation.sh
+++ ci/generate-documentation.sh
@@ -10,8 +10,3 @@ mvn clean install -DskipTests -f $HOME/pitest -q -B > $HOME/pitest-build.log
 # FIXME: mutation testing produces surefire and failsafe reports => must build, test and generate site again...
 ./mvnw install post-site -Dmaven.test.redirectTestOutputToFile=true -Dsurefire.useSystemClassLoader=false -Dfailsafe.useSystemClassLoader=false -B
 
-# copy generated site and reports to a specific folder for deployment
-mkdir -p target/surge/$CIRCLE_BRANCH
-cp -r target/site/* target/surge/$CIRCLE_BRANCH
-echo "surge directory content"
-ls -l target/surge/$CIRCLE_BRANCH
