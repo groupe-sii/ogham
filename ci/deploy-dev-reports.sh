9a9661d26b57be99a27af995256c994be21fca4a
abaudet <abaudet@sii.fr>
2019-07-05 23:19:46 +0400

    chore(tools) Uniq branch names for reports and add variable for output site directory

--- ci/deploy-dev-reports.sh
+++ ci/deploy-dev-reports.sh
@@ -10,15 +10,15 @@ git clone git@github.com:aurelien-baudet/ogham-dev-reports.git $REPORTS_DIR
 # copy generated site
 mkdir -p $REPORTS_DIR/$CIRCLE_BRANCH && cp -r target/site/* $REPORTS_DIR/$CIRCLE_BRANCH
 # update main index to list Ogham branches
+branchnames=$(git for-each-ref --format='%(refname:short)' | while read -r branch; do echo "${branch#origin/}"; done | sort | uniq)
 links=""
 while read branch; do
-	dirname="${branch#origin/}"
-	if [ -d "$REPORTS_DIR/$dirname" ]; then
-		echo "$dirname"
-		links="$links <a href='$dirname'>$dirname</a>"
+	if [ -d "$REPORTS_DIR/$branch" ]; then
+		echo "$branch"
+		links="$links <a href='$branch'>$branch</a>"
 	fi
 done << EOF
-$(git for-each-ref --format='%(refname:short)')
+$(echo "$branchnames")
 EOF
 echo "<html><head></head><body>$links</body>" > $REPORTS_DIR/index.html
 # TODO: remove old branches ?
