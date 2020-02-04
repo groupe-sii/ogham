991fe4930ea0b5c58eed042b069113597b4a5df0
abaudet <abaudet@sii.fr>
2020-02-04 18:44:19 +0400

    chore(ci) Use gregor engine for PIT on CircleCI

--- ci/run-single-module-mutation-tests.sh
+++ ci/run-single-module-mutation-tests.sh
@@ -3,5 +3,8 @@
 set -ex
 
 module="$1"
+engine="${2:-descartes}"
+features=$([ "$engine" = "gregor" ] && echo '-Dfeatures=+EXPORT'  || echo '')
 
-./mvnw install post-site -DtargetModules="$module" -Dpit.report.skip=true -Pmutation-testing-only -Dmaven.test.redirectTestOutputToFile=true -Dsurefire.useSystemClassLoader=false -Dfailsafe.useSystemClassLoader=false -B
+
+./mvnw install post-site -DtargetModules="$module" -Dpit.mutation-engine="$engine" "$features" -Dpit.report.skip=true -Pmutation-testing-only -Dmaven.test.redirectTestOutputToFile=true -Dsurefire.useSystemClassLoader=false -Dfailsafe.useSystemClassLoader=false -B
