0d3c4440bddef6eb1da6dda29c4d225be4e2b576
abaudet <abaudet@sii.fr>
2020-04-02 16:50:37 +0400

    doc(sms) Document session management strategies
    - Document always new strategy
    - Document reuse session strategy
    - Document keep alive strategy
    
    Other:
    - Update BasicSampleExternalProperties
    - Fix diagram paths when using Asciidoctor in Eclipse
    - Add general configuration for plantuml

--- sample-standard-usage/src/main/java/fr/sii/ogham/sample/standard/sms/BasicSampleExternalProperties.java
+++ sample-standard-usage/src/main/java/fr/sii/ogham/sample/standard/sms/BasicSampleExternalProperties.java
@@ -1,7 +1,6 @@
 package fr.sii.ogham.sample.standard.sms;
 
 import java.io.IOException;
-import java.util.Properties;
 
 import fr.sii.ogham.core.builder.MessagingBuilder;
 import fr.sii.ogham.core.exception.MessagingException;
@@ -11,14 +10,12 @@ import fr.sii.ogham.sms.message.Sms;
 public class BasicSampleExternalProperties {
 
 	public static void main(String[] args) throws MessagingException, IOException {
-		// load properties (available at src/main/resources)
-		Properties properties = new Properties();
-		properties.load(BasicSampleExternalProperties.class.getResourceAsStream("/sms.properties"));
 		// Instantiate the messaging service using default behavior and
 		// provided properties
+		// Load properties from file that is in classpath (src/main/resources/sms.properties)
 		MessagingService service = MessagingBuilder.standard()
 				.environment()
-					.properties(properties)
+					.properties("/sms.properties")                 // <1>
 					.and()
 				.build();
 		// send the sms using fluent API
