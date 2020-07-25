0127f39c22ebcfd74c2301bdd5a6dd5c7378e54f
abaudet <abaudet@sii.fr>
2020-07-25 16:58:43 +0400

    ci(pit) Fix PIT report aggregation
    - Update PIT to 1.5.3 (use Ogham branch)
    - Fix Javadoc generation

--- ci/build-pitest.sh
+++ ci/build-pitest.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # build pitest fix report aggregation
-git clone --branch fix/report-aggregation https://github.com/aurelien-baudet/pitest.git $HOME/pitest
+git clone --branch ogham https://github.com/aurelien-baudet/pitest.git $HOME/pitest
 echo "Building pitest..."
 cd $HOME/pitest
 mvn clean install -DskipTests -B
