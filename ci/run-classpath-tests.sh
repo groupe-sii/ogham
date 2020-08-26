4e5391a53088d3797693b7e1fc69fb2e27e577d5
abaudet <abaudet@sii.fr>
2020-08-26 16:28:26 +0400

    ci(timeout) Split mutation tests on CircleCI to bypass timeout

--- ci/run-classpath-tests.sh
+++ ci/run-classpath-tests.sh
@@ -2,5 +2,13 @@
 
 set -ex
 
+dir="${ROOT_TEST_DIR:-$HOME/classpath-tests}"
+
 # TODO: can't use -T 2.5C anymore due to fixed ports in some tests
-$HOME/classpath-tests/$TEST_FOLDER/./mvnw verify -f "$HOME/classpath-tests/$TEST_FOLDER/pom.xml" -Dsurefire.rerunFailingTestsCount=3 -Dfailsafe.rerunFailingTestsCount=3 -Dmaven.javadoc.skip=true -Dmaven.test.redirectTestOutputToFile=true -fae -B $* #| grep -Ei '(error|exception|Tests run:)'
+$dir/$TEST_FOLDER/./mvnw verify -f "$dir/$TEST_FOLDER/pom.xml" \
+	-Dsurefire.rerunFailingTestsCount=3 \
+	-Dfailsafe.rerunFailingTestsCount=3 \
+	-Dmaven.javadoc.skip=true \
+	-Dmaven.test.redirectTestOutputToFile=true \
+	-fae \
+	-B $*
--- ci/run-single-module-mutation-tests.sh
+++ ci/run-single-module-mutation-tests.sh
@@ -4,7 +4,19 @@ set -ex
 
 module="$1"
 engine="${2:-descartes}"
+mutators=$([ -z "$3" ] && echo ''  || echo "-Dmutators=$3")
 features=$([ "$engine" = "gregor" ] && echo '-Dfeatures=+EXPORT'  || echo '')
+history_dir="${4:-/tmp}"
 
-
-./mvnw install post-site -DtargetModules="$module" -Dpit.mutation-engine="$engine" "$features" -Dpit.report.skip=true -Pmutation-testing-only -Dmaven.test.redirectTestOutputToFile=true -Dsurefire.useSystemClassLoader=false -Dfailsafe.useSystemClassLoader=false -B
+./mvnw install post-site \
+	-DtargetModules="$module" \
+	-Dpit.mutation-engine="$engine" \
+	$features \
+	$mutators \
+	-Dpit.history.dir="$history_dir" \
+	-Dpit.report.skip=true \
+	-Pmutation-testing-only \
+	-Dmaven.test.redirectTestOutputToFile=true \
+	-Dsurefire.useSystemClassLoader=false \
+	-Dfailsafe.useSystemClassLoader=false \
+	-B
