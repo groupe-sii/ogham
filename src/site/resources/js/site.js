cb303d2168a5f2a0bca69b4b5b92bdb3de90cfab
abaudet <abaudet@sii.fr>
2021-01-20 09:12:17 +0400

    ci(site) Fix version while generating docs + fix version urls

--- src/site/resources/js/site.js
+++ src/site/resources/js/site.js
@@ -1,13 +1,13 @@
 (function() {
 	
 	var addVersions = function() {
-		var currentVersion = $('.projectVersion').text().replace(/Version: (.+)[.] $/, "$1");
+		var currentVersion = $("a[title='[current-version]'").attr('href');
 		var ul = $("a[title='[version]']").parent().parent();
 		$('.dropdown-toggle', ul).html('v' + currentVersion +' <b class="caret"></b>');
 		$('.dropdown-menu', ul).empty();
 		$.get($(".navbar-brand").attr("href") + "/versions.json", function(data) {
 			data.forEach(function(version) {
-				$('.dropdown-menu', ul).append('<li><a class="dropdown-item" href="'+$(".brand").attr("href")+'/'+version+'/" title="'+version+'">'+version+'</a></li>');
+				$('.dropdown-menu', ul).append('<li><a class="dropdown-item" href="'+$(".navbar-brand").attr("href")+'/'+version+'/" title="'+version+'">'+version+'</a></li>');
 			});
 		});
 	}
